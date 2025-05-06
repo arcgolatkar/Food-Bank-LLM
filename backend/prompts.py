b2122c9bc7b2885b57454cf894513cac9a9b5595
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2025-05-06 08:50:26 +0530

    Update prompts.py

--- backend/prompts.py
+++ backend/prompts.py
@@ -48,8 +48,7 @@ string for it
 
 
 ** Example Input **
-I live near Manassas City Mary Bull Run Unitarian Universalists 9350 Main 
-Street Manassas VA 20110 and I want to go to a food distribution center on Fridays only and I can only go
+I live near Ward 5 Mary House 4303 13th st NE Washington DC 20017 and I want to go to a food distribution center on Fridays only and I can only go
 via walking.
 
 ** Example Output **
